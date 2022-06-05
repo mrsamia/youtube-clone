4a3ea26eee739c248dc5d29bd451a0facac39c01
mrsamia <15203060@iubat.edu>
2022-06-06 00:01:34 +0600

    SRM | sidebar

--- src/Components/Sidebar/Items.js
+++ src/Components/Sidebar/Items.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import styles from './styles.module.css';
 
 function Items(props) {
+
+    function clickHandler(name){
+        alert(name)
+    }
+
     return (
-        <div>
-            <div>
+        <div className={styles.itemsWrapper}>
+            <div className={styles.heading}>
                 <h4>{props.heading}</h4>
             </div>
         <div>
-            <p>{props.items}</p>
+            <p>{props.items.map((c)=>{
+                return(
+                    <div className={styles.items}>
+                        <p onClick={() => clickHandler(c.name)}>{c.name}</p>
+                    </div>
+                );
+            })}</p>
         </div>
         </div>
     );
--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import data from '../../data.json';
 import Items from './Items';
+import styles from './styles.module.css';
 
 function Sidebar(props) {
-    
-    return data.sidebar.map((e) => {
-        return (
-            <Items heading={e.heading} 
-           
-            />
-        );
-    })
+
+    return (
+        <div className={styles.sidebarWrapper}>
+            {data.sidebar.map((e) => {
+                return (
+                    <Items heading={e.heading}
+                        items={e.items}
+                    />
+                );
+            })}
+        </div>
+    )
+
 }
 
 export default Sidebar;
